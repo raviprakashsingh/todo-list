8cece4bc15ff51bb7e21b291b13305abe2af09c9
Ravi Prakash Singh <Ravip@cloudthing.com>
2019-12-19 10:26:31 +0530

    Updates

---
+++ precache-manifest.4ecaae537c271a9dc901a5789b6b1a7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f15700235ec9df1543bac8848e37b687",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "69ddfe203a483531b42d",
+    "url": "/todo-list/static/css/main.dd193482.chunk.css"
+  },
+  {
+    "revision": "509730b2928db7970827",
+    "url": "/todo-list/static/js/2.941f4f02.chunk.js"
+  },
+  {
+    "revision": "1d7c3211cc0e2191d7bbe9b3709c2276",
+    "url": "/todo-list/static/js/2.941f4f02.chunk.js.LICENSE"
+  },
+  {
+    "revision": "69ddfe203a483531b42d",
+    "url": "/todo-list/static/js/main.5b6ab0f0.chunk.js"
+  },
+  {
+    "revision": "d9b4caacda682ac1633d",
+    "url": "/todo-list/static/js/runtime-main.20e96aa4.js"
+  }
+]);
